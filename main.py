0b35caafa79ca1081feec807a441511e1e4c3747
Karen Gomes <karen.nayara@audora.com.br>
2019-05-31 14:17:34 -0300

    get each title post

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ res.encoding = "utf-8"
 soup = BeautifulSoup(res.text, 'html.parser')
 
 posts = soup.find_all(class_='post')
+all_posts = []
 
 for post in posts:
-    print(post.find('h2').text)
+    info = post.find(class_='post-content')
+    title = info.h2.text
+    print(title)
