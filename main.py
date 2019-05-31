ac2f4904c9761c9bce039bd3f10cfed50328e9c4
Karen Gomes <karen.nayara@audora.com.br>
2019-05-31 15:15:10 -0300

    get all posts from all pages

--- main.py
+++ main.py
@@ -10,26 +10,25 @@ soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.find(class_='pagination').find_all('a')
 
 all_pages = []
+all_posts = []
+
 for link in links:
     page = requests.get(link.get('href'))
     page.encoding = 'utf-8'
-    all_pages.append(BeautifulSoup(page.text, 'html.parser'))
+    all_pages.append(BeautifulSoup(
+        page.text, 'html.parser').find_all(class_='post'))
 
-print(len(all_pages))
 
-posts = soup.find_all(class_='post')
-
-all_posts = []
-
-for post in posts:
-    info = post.find(class_='post-content')
-    title = info.h2.text
-    preview = info.p.text
-    author = info.find(class_='post-author').text[5:]
-    time = info.footer.time['datetime']
-    img = post.find(class_='wp-post-image')['src']
-    all_posts.append({'title': title, 'preview': preview,
-                      'author': author, 'time': time, 'img': img})
+for posts in all_pages:
+    for post in posts:
+        info = post.find(class_='post-content')
+        title = info.h2.text
+        preview = info.p.text
+        author = info.find(class_='post-author').text[5:]
+        time = info.footer.time['datetime']
+        img = post.find(class_='wp-post-image')['src']
+        all_posts.append({'title': title, 'preview': preview,
+                          'author': author, 'time': time, 'img': img})
 
 with open('posts.json', 'w') as json_file:
     json.dump(all_posts, json_file, indent=3, ensure_ascii=False)
